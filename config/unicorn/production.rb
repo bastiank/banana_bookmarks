8fe7b33711cd90591ab0422561a30758add76e79
Bastian Kriesten <bastian.kriesten@mobileeventguide.de>
2012-05-10 12:35:26 +0200

    unicorn config added

---
+++ config/unicorn/production.rb
@@ -0,0 +1,8 @@
+working_directory "/home/bastian/rails_apps/bookmarks/current"
+pid "/home/bastian/rails_apps/bookmarks/current/tmp/pids/unicorn.pid"
+stderr_path "/home/bastian/rails_apps/bookmarks/shared/log/unicorn.log"
+stdout_path "/home/bastian/rails_apps/bookmarks/shared/log/unicorn.log"
+
+listen 5232 #"/tmp/seroki_devel_unicorn.sock"
+worker_processes 2
+timeout 30
